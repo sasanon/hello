0aa7b9e057982554110ab5b4e1b7d02365911162
sasanon <sasa.milkcocoa@gmail.com>
2014-05-24 16:30:21 +0900

    forを使わずに10回Hello World

--- hello.c
+++ hello.c
@@ -2,9 +2,15 @@
 
 int main()
 {
-	int i;
-	for (i = 0; i < 10; i++) {
-		printf("Hello World\n");
-	}
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
 	return 0;
 }
